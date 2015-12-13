568b73e02fdf5b90b5a4d4bcd7eddb792f257204
Daniel Ourada <daniel.ourada@gmail.com>
2015-12-13 14:18:42 -0800

    initial checkin

---
+++ DSIClientX.csharp/DSIClientX.csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSIClientX.csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dsi = new DSICLIENTXLib.DSICLientX();
+            var response = dsi.ServerIPConfig("x1.mercurycert.net", 0);
+            Console.WriteLine(response);
+            Console.ReadLine();
+
+        }
+    }
+}
