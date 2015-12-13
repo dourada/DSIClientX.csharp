5eea8aaa7064bb474f908a534b8790ec76a8c536
Daniel Ourada <daniel.ourada@gmail.com>
2015-12-13 14:25:51 -0800

    added unit tests

---
+++ DSIClientX.csharp/DSIClientX.Test/Calculator.Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DSIClientX.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Calculator_Addition_Success_Test()
+        {
+            var expected = 4;
+            var calc = new DSIClientX.csharp.Calculator();
+            var result = calc.Add(2, 2);
+            Assert.IsTrue(result == expected);
+        }
+
+        [TestMethod]
+        public void Calculator_Addition_Failure_Test()
+        {
+            var expected = 2;
+            var calc = new DSIClientX.csharp.Calculator();
+            var result = calc.Add(2, 2);
+            Assert.IsTrue(result == expected);
+        }
+
+        [TestMethod]
+        public void Calculator_Multiplication_Success_Test()
+        {
+            var expected = 4;
+            var calc = new DSIClientX.csharp.Calculator();
+            var result = calc.Multiplication(2, 2);
+            Assert.IsTrue(result == expected);
+        }
+
+    }
+}
---
+++ DSIClientX.csharp/DSIClientX.csharp/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSIClientX.csharp
+{
+    public class Calculator
+    {
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
